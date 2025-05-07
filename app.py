8067b5f7ac213b5a0341e50bc0a9fd572f763d3c
Eduardo-J-Morales <root@DESKTOP-RHJBB6F.>
2025-05-07 09:34:56 -0400

    Updates on app.py and Installed flask

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask import Flask, requiest, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource, reqparse
 from flask_httpauth import HTTPBasicAuth
-from datetime import datetime
-from passlib.hash import bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
 
+app = Falsk(__name__)
+api = Api(app)
+
+db = SQLAlchemy(app)
+
+auth = HTTPBasicAuth()
+
+users = {
+    "admin": generate_password_hash("password123")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+    
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+    published_date = db.Column(db.String(10), nullable=False)
+    isbn = db.Column(db.String(13), unique=True, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'published_date': self.published_date,
+        }
+    
+def validate_book_data(data):
+    if 
